3a70712265910ed5c262b3a15f6ef5a48bac4a2e
mohdishaq786 <ishaqmohd50@gmail.com>
2025-06-22 06:31:15 +0530

    Add:new model in background remover and remove sharperness  in overlap

--- services/super_imposer.py
+++ services/super_imposer.py
@@ -2,8 +2,10 @@ from PIL import Image
 
 def superimpose_with_resize(background: Image.Image, overlay: Image.Image) -> Image.Image:
     # Ensure both images have alpha channels
-    background = background.convert("RGBA")
+    background = background.convert("RGBA")#.resize([3840, 720])
+    background.thumbnail([1920, 1080])
     overlay = overlay.convert("RGBA")
+    overlay.thumbnail([1920, 1080])
 
     # Get areas
     bg_width, bg_height = background.size
